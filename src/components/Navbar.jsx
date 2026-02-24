1652143d8879d6fd64d15068419aa3ac149051d4
Keira Lavelle <150979394+keiralavelle@users.noreply.github.com>
2026-02-24 17:17:01 -0500

    Lab 12

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import styles from "../styles/navbar.module.css";
+import { Link } from "react-router-dom";
 import { useContext } from "react";
 import ModeContext from "../context/ModeContext";
 
@@ -5,9 +7,20 @@ const Navbar = () => {
   const { theme, toggleTheme } = useContext(ModeContext);
 
   return (
-    <nav>
-      <button onClick={toggleTheme}>
-        Theme: {theme}
+    <nav className={styles.navbar}>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/add-profile">Add Profile</Link></li>
+        <li><Link to="/fetched-profiles">Other Profiles</Link></li>
+      </ul>
+
+      <button
+        type="button"
+        className={styles.themeToggle}
+        onClick={toggleTheme}
+      >
+        Switch to {theme === "light" ? "Dark" : "Light"} Mode
       </button>
     </nav>
   );
