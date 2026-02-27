62c70a7bb71af945703a1f6be6c5a63cd2919b56
Keira Lavelle <150979394+keiralavelle@users.noreply.github.com>
2026-02-27 16:14:20 -0500

    PLEASE WORK

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,23 +1,27 @@
+import { memo } from "react";
+import styles from "../styles/Filters.module.css";
 
-const Filters = memo(({titles,title, name, handleChange, handleSearch, handleClick}) => {
-    return (
-        <div className={styles.filterContainer}>
-            <div className={styles.filterDropdown}>
-                <label htmlFor="title">Select a title</label>
-                <select id="title" onChange={handleChange} value={title}>
-                    <option value="">All</option>
-                    {
-                        titles.map(title => <option key={title} value={title}>{title}</option>)
-                    }
-                </select>
-            </div>
-            <div className={styles.filterSearch}>
-                <label htmlFor="search">Search a name</label>
-                <input id="search" onChange={handleSearch} value={name}/>
-            </div>
-            <button onClick={handleClick}>Clear</button>
-        </div>
-    )
+const Filters = memo(({ titles, title, name, handleChange, handleSearch, handleClick }) => {
+  return (
+    <div className={styles.filterContainer}>
+      <div className={styles.filterDropdown}>
+        <label htmlFor="title">Select a title</label>
+        <select id="title" onChange={handleChange} value={title}>
+          <option value="">All</option>
+          {titles.map(title => (
+            <option key={title} value={title}>{title}</option>
+          ))}
+        </select>
+      </div>
 
-})
+      <div className={styles.filterSearch}>
+        <label htmlFor="search">Search a name</label>
+        <input id="search" onChange={handleSearch} value={name}/>
+      </div>
+
+      <button onClick={handleClick}>Clear</button>
+    </div>
+  );
+});
+
 export default Filters;
