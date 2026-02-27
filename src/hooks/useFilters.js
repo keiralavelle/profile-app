c2c6bfdae13494a8670594ac079123e52112ee7d
Keira Lavelle <150979394+keiralavelle@users.noreply.github.com>
2026-02-26 23:50:08 -0500

    lab 12

--- src/hooks/useFilters.js
+++ src/hooks/useFilters.js
@@ -1,9 +1,29 @@
-import {useState, useCallBack} from "react"; 
-const useFilters = 
+import { useState, useCallback } from "react";
 
-const [title, setTitle] = useState("");
-const [name, setName] = useState("");
-const handleChangeTitle = useCallBack((event) => {
+const useFilters = () => {
+  const [title, setTitle] = useState("");
+  const [name, setName] = useState("");
+
+  const handleChangeTitle = useCallback((event) => {
     setTitle(event.target.value);
-    }, []);
-export default useFilters; 
+  }, []);
+
+  const handleSearch = useCallback((event) => {
+    setName(event.target.value);
+  }, []);
+
+  const handleClear = useCallback(() => {
+    setTitle("");
+    setName("");
+  }, []);
+
+  return {
+    title,
+    name,
+    handleChangeTitle,
+    handleSearch,
+    handleClear,
+  };
+};
+
+export default useFilters;
